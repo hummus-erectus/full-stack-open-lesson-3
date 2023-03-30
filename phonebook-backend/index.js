cf680b3dc18844330d2f96792dd56a9200e9a3e2
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-03-30 12:23:36 +0900

    Complete 3.19
    Expand the validation so that the name stored in the database has to be at least three characters long.
    
    Expand the frontend so that it displays some form of error message when a validation error occurs.

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -51,47 +51,42 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 
   const body = request.body
 
-  if (body.name === undefined) {
-    return response.status(400).json({
-      error: 'Name field is missing'
-    })
-  }
+  // if (body.name === undefined) {
+  //   return response.status(400).json({
+  //     error: 'Name field is missing'
+  //   })
+  // }
 
-  if (body.number === undefined) {
-    return response.status(400).json({
-      error: 'Number field is missing'
-    })
-  }
+  // if (body.number === undefined) {
+  //   return response.status(400).json({
+  //     error: 'Number field is missing'
+  //   })
+  // }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  if (persons.find(p => p.name === person.name)){
-    return response.status(400).json({
-      error: 'A person with that name already exists'
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-  }
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number},
+    {new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -110,7 +105,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
